a4ebb6b4987d73715ab3b94231d297ae25d16a9b
Victor Schröder <schrodervictor@gmail.com>
2016-06-01 01:41:14 +0200

    Extends the ATranslator to handle not yet mapped labels

--- src/assembler/ATranslator.erl
+++ src/assembler/ATranslator.erl
@@ -14,5 +14,12 @@ integerToBinary(Num) -> io_lib:format("~16..0s", [integer_to_list(Num, 2)]).
 symbolToBinary(Label, SymbolTable) ->
     case dict:find(Label, SymbolTable) of
         {ok, Value} -> {integerToBinary(Value), SymbolTable};
-        _ -> {error, not_implemented}
+        _ ->
+            UpdatedTable = allocate_symbol(Label, SymbolTable),
+            symbolToBinary(Label, UpdatedTable)
     end.
+
+allocate_symbol(Label, SymbolTable) ->
+    {ok, Value} = dict:find("__next__", SymbolTable),
+    UpdatedSymbolTable = dict:store("__next__", Value + 1, SymbolTable),
+    dict:store(Label, Value, UpdatedSymbolTable).
--- tests/assembler/ATranslator_tests.erl
+++ tests/assembler/ATranslator_tests.erl
@@ -11,13 +11,14 @@ translate_test_() ->
         fun translate_setup/0,
         fun(SetupData) -> [
             translate_handles_numeric(SetupData),
-            translate_handles_labeled_existent(SetupData)
+            translate_handles_labeled_existent(SetupData),
+            translate_handles_labeled_not_existent(SetupData)
         ] end
     }.
 
 translate_setup() ->
     ATranslate = fun 'ATranslator':translate/2,
-    SymbolTable = dict:from_list([{"LOOP", 10}]),
+    SymbolTable = dict:from_list([{"__next__", 16}, {"LOOP", 10}]),
     {ATranslate, SymbolTable}.
 
 translate_handles_numeric({ATranslate, SymbolTable}) ->
@@ -38,4 +39,30 @@ translate_handles_labeled_existent({ATranslate, SymbolTable}) ->
     [
         ?_assertBinaryEqual("0000000000001010", Result0),
         ?_assertEqual(SymbolTable, SymbolTable0)
+    ].
+
+translate_handles_labeled_not_existent({ATranslate, SymbolTable}) ->
+    {ok, {Result0, SymbolTable0}} = ATranslate("@var", SymbolTable),
+    {ok, {Result1, SymbolTable1}} = ATranslate("@foo", SymbolTable0),
+    {ok, {Result2, SymbolTable2}} = ATranslate("@bar", SymbolTable1),
+    {ok, {Result3, SymbolTable3}} = ATranslate("@var", SymbolTable2),
+    [
+        ?_assertBinaryEqual("0000000000010000", Result0),
+        ?_assertBinaryEqual("0000000000010001", Result1),
+        ?_assertBinaryEqual("0000000000010010", Result2),
+        ?_assertBinaryEqual("0000000000010000", Result3),
+
+        ?_assertEqual({ok, 16}, dict:find("var", SymbolTable0)),
+        ?_assertEqual({ok, 16}, dict:find("var", SymbolTable1)),
+        ?_assertEqual({ok, 16}, dict:find("var", SymbolTable2)),
+        ?_assertEqual({ok, 16}, dict:find("var", SymbolTable3)),
+
+        ?_assertEqual({ok, 17}, dict:find("foo", SymbolTable1)),
+        ?_assertEqual({ok, 17}, dict:find("foo", SymbolTable2)),
+        ?_assertEqual({ok, 17}, dict:find("foo", SymbolTable3)),
+
+        ?_assertEqual({ok, 18}, dict:find("bar", SymbolTable2)),
+        ?_assertEqual({ok, 18}, dict:find("bar", SymbolTable3)),
+
+        ?_assertEqual(SymbolTable2, SymbolTable3)
     ].
